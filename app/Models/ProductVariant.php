5f718e1b6b2aa130b27112aef03be356cfe12ab5
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-04-07 23:26:47 +0600

    Update `ProductVariant` model with soft deletes, factory, and explicit table/primary key definitions.

--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductVariant extends Model
 {
+    use SoftDeletes, HasFactory;
+
+    protected $table = 'products_variants';
+    protected $primaryKey = 'productVariantId';
+
     protected $fillable = [
         'sku',
         'productId',
