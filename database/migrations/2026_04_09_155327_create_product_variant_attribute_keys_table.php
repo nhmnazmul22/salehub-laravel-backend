275e5bb6855451a881a289f3c54c7963d6804cad
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-04-09 22:23:07 +0600

    Add `product_variant_attribute_keys` and `product_variant_attribute_values` tables with soft deletes and foreign key constraints.

---
+++ database/migrations/2026_04_09_155327_create_product_variant_attribute_keys_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variant_attribute_keys', function (Blueprint $table) {
+            $table->id('variantAttributeKeyId');
+            $table->string('key');
+            $table->foreignId('productVariantId')
+                ->constrained('product_variants', 'productVariantId')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variant_attributes');
+    }
+};
---
+++ database/migrations/2026_04_09_161930_create_product_variant_attribute_values_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variant_attribute_values', function (Blueprint $table) {
+            $table->id('variantAttributeValueId');
+            $table->string('value');
+            $table->foreignId('variantAttributeKeyId')
+                ->constrained('product_variant_attribute_keys', 'variantAttributeKeyId')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variant_attribute_values');
+    }
+};
