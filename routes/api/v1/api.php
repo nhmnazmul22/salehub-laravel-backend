da8286bac931da20a7cc7fc131ca2c0132baf6c3
Nhm Nazmul <najmulmonir12345@gmail.com>
2026-04-29 11:28:06 +0600

    Simplify auth route grouping and move refresh token to public routes for better security isolation.

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -4,19 +4,17 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Middleware\RefreshTokenMiddleware;
 
-Route::middleware('auth:api')->prefix('v1')->as('v1.')->group(function () {
-    Route::prefix('auth')->as('auth.')->group(function () {
-        Route::withoutMiddleware('auth:api')->group(function () {
-            Route::post('/login', [AuthController::class, 'login'])->name('login');
-            Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
-            Route::post('/verify-otp', [AuthController::class, 'verifyOTP'])->name('verify-otp');
-            Route::patch('/reset-password', [AuthController::class, 'resetPassword'])->name('reset-password');
-            Route::post('/refresh-token', [AuthController::class, 'refreshToken'])->name('refresh-token')
-                ->middleware(RefreshTokenMiddleware::class);
-        });
-
+Route::prefix('v1/auth')->as('v1.auth.')->group(function () {
+    Route::middleware('auth:api')->group(function () {
         Route::get('/me', [AuthController::class, 'me'])->name('me');
         Route::post('/logout', [AuthController::class, 'logOut'])->name('logout');
         Route::patch('/change-password', [AuthController::class, 'changePassword'])->name('change-password');
     });
+
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
+    Route::post('/verify-otp', [AuthController::class, 'verifyOTP'])->name('verify-otp');
+    Route::patch('/reset-password', [AuthController::class, 'resetPassword'])->name('reset-password');
+    Route::get('/refresh-token', [AuthController::class, 'refreshToken'])->name('refresh-token')
+        ->middleware(RefreshTokenMiddleware::class);
 });
